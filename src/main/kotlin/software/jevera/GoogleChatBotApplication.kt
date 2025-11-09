261312d4a742716d667ba2fd57cb7095a73d1796
ssenko <sergeysenja1992@gmail.com>
2025-11-09 17:22:12 +0200

    Init commit

---
+++ src/main/kotlin/software/jevera/GoogleChatBotApplication.kt
@@ -0,0 +1,11 @@
+package software.jevera
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleChatBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<GoogleChatBotApplication>(*args)
+}
---
+++ src/test/kotlin/software/jevera/GoogleChatBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package software.jevera
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoogleChatBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
